337881fda698df3993a5efd061e996beeb9d6292
OzzmanUA <Ozzman.zp@gmail.com>
2022-09-24 13:09:06 +0300

    Add programer class

---
+++ Programer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Programer : Worker
+    {
+        public int Str { get; set; }
+        public override double GetResultSalary()
+        {
+            return Salary * Str;
+        }
+    }
+}
